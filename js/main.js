914a4095dc49dd17e768b213a4b406f298e91448
Luther <lutherking30@gmail.com>
2021-09-12 16:20:55 +0300

    All what-we-do icons set on toggle

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@
 $(document).ready(function(){
     $("#dev-image").click(function(){
         $(".development").toggle();
+    });
+
+    $("#product-image").click(function(){
+        $(".products").toggle();
+    })
+
+    $("#design-image").click(function(){
+        $(".design").toggle();
     })
    
 });
